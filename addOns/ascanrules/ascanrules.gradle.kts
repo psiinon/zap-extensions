24df1ac61c341f5c9774ca23a56c81fba931bf5f
Simon Bennetts <psiinon@gmail.com>
2022-10-18 12:42:42 +0200

    ascanRules: Promote beta to release
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- addOns/ascanrules/ascanrules.gradle.kts
+++ addOns/ascanrules/ascanrules.gradle.kts
@@ -16,6 +16,9 @@ zapAddOn {
                 register("commonlib") {
                     version.set(">= 1.9.0 & < 2.0.0")
                 }
+                register("oast") {
+                    version.set(">= 0.7.0")
+                }
             }
         }
     }
@@ -23,11 +26,15 @@ zapAddOn {
 
 dependencies {
     compileOnly(parent!!.childProjects.get("commonlib")!!)
+    compileOnly(parent!!.childProjects.get("custompayloads")!!)
+    compileOnly(parent!!.childProjects.get("oast")!!)
     implementation("com.googlecode.java-diff-utils:diffutils:1.3.0")
     implementation("org.bitbucket.mstrobel:procyon-compilertools:0.5.36")
 
     testImplementation(parent!!.childProjects.get("commonlib")!!)
     testImplementation(parent!!.childProjects.get("commonlib")!!.sourceSets.test.get().output)
+    testImplementation(parent!!.childProjects.get("custompayloads")!!)
+    testImplementation(parent!!.childProjects.get("oast")!!)
     testImplementation(project(":testutils"))
 }
 
